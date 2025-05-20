0445da66b275705b8bbd530b1ab6b90ebc597467
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-20 23:11:07 +0200

    Add parser and prompt template to use

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
-from langchain_core.prompts import AnthropicPrompts
-from langchain_core.output_parsers import PythonOutputParser
+from langchain_core.prompts import AnthropicPrompts, ChatPromptTemplate
+from langchain_core.output_parsers import PythonOutputParser, PydanticOutputParser
 
 load_dotenv()
 
@@ -13,4 +13,20 @@ class ResearchResponse(BaseModel):
     tools_used: list[str]
 
 llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
