ebf2dbe9b4c8ebdb239f080dd77a7f1abe5bf205
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-20 23:07:27 +0200

    Add class ResearchResponse

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import AnthropicPrompts
+from langchain_core.output_parsers import PythonOutputParser
 
 load_dotenv()
 
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
 llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
-response = llm.invoke("What is a Bydgoszcz?")
-print(response)
+
