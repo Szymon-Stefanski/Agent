7e71677ee21390681597f2f619bdd5486fa9cbcc
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-21 23:32:35 +0200

    Add parsing to the response

--- main.py
+++ main.py
@@ -40,4 +40,8 @@ agent = create_tool_calling_agent(
 
 agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
 raw_response = agent_executor.invoke({"query":"What is the capital of Japan?"})
-print(raw_response)
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+except Exception as e:
+    print("Error parsing response", e, "Raw response: ", raw_response)
