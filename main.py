42fb5e390f46ac320b4b3d320eec51a83e110c9b
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-22 23:06:41 +0200

    Code refactor - delete unused package and name duplicate, sort requirements

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser, PydanticOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
 from tools import search_tool, wiki_tool
 
--- tools.py
+++ tools.py
@@ -2,7 +2,6 @@ from langchain_community.tools import WikipediaQueryRun
 from langchain_google_community import GoogleSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
-from datetime import datetime
 
 search = GoogleSearchRun
 search_tool = Tool(
